52c587fe5db45140d0356d9481fa17a93f7024bb
Sheila-source <73207169+Sheila-source@users.noreply.github.com>
2021-03-19 20:18:17 +0800

    Delete Laravel_Project directory

--- Laravel_Project/public/js/script.js
+++
@@ -1,6 +0,0 @@
-$('#deleteCategory').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var action = button.data('action');
-    var modal = $(this);
-    modal.find('form').attr('action', action);
-});
